731b0c5856d29ec28f181c39c518ef0f4bccc451
mawarutuco <xul4841268@gmail.com>
2022-03-02 13:01:23 +0800

    2022/3/2-2

--- 1008/1008.js
+++ 1008/1008.js
@@ -16,15 +16,39 @@ rl.on('close', function () {
 })
 
 function solve(lines) {
-    let water = Number(lines)
+
+    let water = Number(lines[0])
     let bucket = 0
     let tmp = 0
     do {
-        for (let i = 1; Math.pow(2, i) <= water; i++) {
+        
+        for (let i = 0; Math.pow(2, i) <= water; i++) {
             tmp = Math.pow(2, i)
         }
         water -= tmp
         bucket++
     } while (water > 0)
     console.log(bucket)
+
 }
+
+// 因地制夷下午12:45
+// function solve(lines) {
+//   let water = Number(lines[0]);
+//   let arr = [];
+//   let counter = 0;
+//   for (let i = 0; i < 31; i++) {
+//     arr.push(2 ** i);
+//   }
+//   for (let i = 30; i >= 0; i--) {
+//     let remainder = 0;
+//     if (water >= arr[i]) {
+//       remainder = water % arr[i];
+//       counter = counter + (water - remainder) / arr[i];
+//       water -= arr[i];
+//     }
+//   }
+
+//   console.log(counter);
+// }
+// ciy-ydeh-jir
