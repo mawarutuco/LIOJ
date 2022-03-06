7ae1af95366364fd612443dff3b2203a5f40cba5
mawarutuco <xul4841268@gmail.com>
2022-03-06 22:26:46 +0800

    2022/3/6-3

---
+++ 1020/1020.js
@@ -0,0 +1,21 @@
+//type input.txt | node 1020.js
+
+var readline = require('readline');
+
+var lines = []
+var rl = readline.createInterface({
+    input: process.stdin
+});
+
+rl.on('line', function (line) {
+    lines.push(line)
+});
+
+rl.on('close', function () {
+    solve(lines)
+})
+
+
+function solve(lines) {
+    
+}
