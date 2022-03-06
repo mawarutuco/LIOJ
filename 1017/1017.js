1eed96d6a8842239cf77c97b87f233176e2a6fda
mawarutuco <xul4841268@gmail.com>
2022-03-06 20:35:34 +0800

    Update 1017.js

--- 1017/1017.js
+++ 1017/1017.js
@@ -1,8 +1,6 @@
 //type input.txt | node 1017.js
 
-// const { copyFileSync } = require('fs');
 var readline = require('readline');
-const { arrayBuffer } = require('stream/consumers');
 
 var lines = []
 var rl = readline.createInterface({
@@ -17,25 +15,28 @@ rl.on('close', function () {
     solve(lines)
 })
 
+//1017
 function solve(lines) {
 
-    let last = Number(lines[1]) + 1
-    let arr = [], value = 0, biggest = 0
+    let arr = [], value = 0, biggest = 0, last = 0
 
-    for (let i = 2; i <= last; i++) {
+    for (let i = 2; i <= Number(lines[1]) + 1; i++) {
         arr.push(Number(lines[i]))
     }
-    // console.log(arr)
+
+    if (Number(lines[0]) > Number[1]) { last = Number(lines[1]) }
+    else { last = Number(lines[0])  }
 
     //判斷偷到物品之總計價值
-    for (let i = 0; i < lines[0]; i++) {
-        if (Math.max(...arr) < 0) { biggest = 0 }
+    for (let i = 0; i < last; i++) {
+        if (Math.max(...arr) == '-Infinity') { biggest = 0 }
         else { biggest = Math.max(...arr) }
         value += biggest
 
         arr.splice(arr.indexOf(biggest), 1)
         //splice(start[,deleteCount])
 
+        // console.log(Math.max(...arr)== '-Infinity')
         // console.log('big:' + biggest + ',arr:' + arr)
         // console.log('----')
     }
