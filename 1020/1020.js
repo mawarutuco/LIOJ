fef95f45ecd7bcb4a8781b0df0340b214967fceb
mawarutuco <xul4841268@gmail.com>
2022-03-07 17:22:44 +0800

    Update 1020.js

--- 1020/1020.js
+++ 1020/1020.js
@@ -17,5 +17,32 @@ rl.on('close', function () {
 
 
 function solve(lines) {
-    
+    //P 是質數，輸出：Prime，否之則輸出 Composite
+
+    let x = 0
+
+    let num = Number(lines[0])
+    for (let i = 1; i <= num; i++) {
+        x = Number(lines[i])
+        isComposite(x)
+    }
+}
+
+function isComposite(x) {
+    switch (x) {
+        case 1:
+            console.log('Composite')
+            return
+        case 2:
+            console.log('Prime')
+            return
+    }
+
+    for (let i = 2; i <= x / 2 + 1; i++) {
+        if (x % i == 0) {
+            console.log('Composite')
+            return
+        }
+    }
+    console.log('Prime')
 }
