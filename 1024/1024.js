0cbc1110c6defed789505fe560c64e8a0a13cc20
mawarutuco <xul4841268@gmail.com>
2022-03-07 22:58:34 +0800

    Update 1024.js

--- 1024/1024.js
+++ 1024/1024.js
@@ -1,6 +1,5 @@
 //type input.txt | node 1024.js
 
-
 var readline = require('readline');
 
 var lines = []
@@ -21,13 +20,10 @@ function solve(lines) {
     let n = Number(lines[0])
     let m = Number(lines[1])
 
-
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= m; j++) {
             console.log(i + '*' + j + '=' + i * j)
         }
-
     }
-
 }
 
