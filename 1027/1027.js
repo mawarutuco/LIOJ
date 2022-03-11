4e12229121bad32b97ce3b1e3ea2a83edb69faed
mawarutuco <xul4841268@gmail.com>
2022-03-11 22:50:26 +0800

    Update 1027.js

--- 1027/1027.js
+++ 1027/1027.js
@@ -19,10 +19,25 @@ rl.on('close', function () {
 function solve(lines) {
 
     let arr = lines[0].split('')
-
+    let sum = 0
     // console.log(arr)
-    for (let i = 0; i < arr.length; i++) {
-        if (Number(arr[i]) % 2 == 0) { Number(arr[i]) * 2 }
+    for (let i = 0; i < arr.length-1; i++) {
+        let num = Number(arr[i]);
+        if (!isNaN(num)) {
+            if (num % 2 != 0) {
+                num = num * 2
+                if (num > 9) { num = num - 9 }
+            }
+            sum += num
+            // console.log('num:' + num + 'sum:' + sum)
+        }
     }
+    let checkNum = 10 - (sum % 10)
+    // console.log('ch:'+checkNum)
+    if (checkNum == arr[18]) {
+        if (arr[0] == 5) { console.log('MASTER_CARD') }
+        else if (arr[0] == 4) { console.log('VISA') }
+    }
+    else { console.log('INVALID') }
 }
 
