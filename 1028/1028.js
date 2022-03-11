f43de4fb8f078ab6a7dfe6001c1cd2ab6126a915
mawarutuco <xul4841268@gmail.com>
2022-03-11 23:43:26 +0800

    Create 1028.js

---
+++ 1028/1028.js
@@ -0,0 +1,42 @@
+//type input.txt | node 1028.js
+
+var readline = require('readline');
+
+var lines = []
+var rl = readline.createInterface({
+    input: process.stdin
+});
+
+rl.on('line', function (line) {
+    lines.push(line)
+});
+
+rl.on('close', function () {
+    solve(lines)
+})
+
+let arr = [], sum = 0
+function solve(lines) {
+
+    let tmp = lines[0].split(' ')
+
+    for (let i = 0; i < tmp.length; i++) {
+        arr = tmp[i].split('')
+        x()
+        arr = []
+    }
+    sum = String(sum)
+    arr = sum.split('')
+    sum = 0
+    x()
+    console.log(sum)
+    
+}
+function x() {
+    
+    for (let j = 0; j < arr.length; j++) {
+        sum += Number(arr[j])
+        // console.log(sum)
+
+    }
+}
